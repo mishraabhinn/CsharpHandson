c0e7bb53398e875766b70b787b24e85a2a4a9555
Abhinn Mishra <aviabhinn98@gmail.com>
2021-05-13 15:53:40 +0530

    MethodOverloading

---
+++ ClassAndObjects/MethodOverloading.cs
@@ -0,0 +1,36 @@
+using System;
+namespace CSharpHandsOnPractice.ClassAndObjects
+{
+class MethodOverLoading {
+
+	// adding three integer values.
+	public int Add(int a, int b, int c)
+	{
+		int sum = a + b + c;
+		return sum;
+	}
+
+	// adding three double values.
+	public double Add(double a,
+					double b, double c)
+	{
+		double sum = a + b + c;
+		return sum;
+	}
+
+	// Main Method
+	public static void Main(String[] args)
+	{
+
+		// Creating Object
+		MethodOverLoading obj = new MethodOverLoading();
+
+		int sum2 = obj.Add(1, 2, 3);
+		Console.WriteLine("sum of the three "
+						+ "integer value : " + sum2);
+		double sum3 = obj.Add(1.0, 2.0, 3.0);
+		Console.WriteLine("sum of the three "
+						+ "double value : " + sum3);
+	}
+}
+}
