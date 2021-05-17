334e61f138239fa7b722e238292d210c3424f55c
Abhinn Mishra <aviabhinn98@gmail.com>
2021-05-17 16:03:28 +0530

    interface

--- ClassAndObjects/Interface.cs
+++ ClassAndObjects/Interface.cs
@@ -7,14 +7,19 @@ namespace CSharpHandOnPractice.ClassAndObjects
     }
     
     class driver:Interface
-    {
+    {   string name;
+        public driver(string name)
+        {
+            this.name=name;
+
+        }
         public void add(int x, int y)
         {
-            Console.WriteLine(x+y);
+            Console.WriteLine(name+"="+x+y);
         }
         static void Main()
         {
-            var obj=new driver();
+            var obj=new driver("Add");
             obj.add(8,2);
         }
     }
