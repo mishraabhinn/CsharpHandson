f0e6140a8744b0e1b61a701599625a13c0ebc228
Abhinn Mishra <aviabhinn98@gmail.com>
2021-05-17 17:17:57 +0530

    Nullable

--- Intermediate/Nullable/MagicNumber/PlayerDisplayer.cs
+++ Intermediate/Nullable/MagicNumber/PlayerDisplayer.cs
@@ -8,27 +8,27 @@ namespace GameConsole
 {
 class PlayerDisplayer
 {
-	public static void Write(PlayerCharacter player)
-	{
-		Console.WriteLine(player.Name);
+    public static void Write(PlayerCharacter player)
+    {
+        Console.WriteLine(player.Name);
 
-		if (player.DaysSinceLastLogin == -1)
-		{
-			Console.WriteLine("No value for DaysSinceLastLogin");
-		}
-		else
-		{
-			Console.WriteLine(player.DaysSinceLastLogin);
-		}
+        if (player.DaysSinceLastLogin == -1)
+        {
+            Console.WriteLine("No value for DaysSinceLastLogin");
+        }
+        else
+        {
+            Console.WriteLine(player.DaysSinceLastLogin);
+        }
 
-		if (player.DateOfBirth == DateTime.MinValue)
-		{
-			Console.WriteLine("No date of birth specified");
-		}
-		else
-		{
-			Console.WriteLine(player.DateOfBirth);
-		}
-	}
+        if (player.DateOfBirth == DateTime.MinValue)
+        {
+            Console.WriteLine("No date of birth specified");
+        }
+        else
+        {
+            Console.WriteLine(player.DateOfBirth);
+        }
+    }
 }
 }
