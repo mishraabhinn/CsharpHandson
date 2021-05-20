9bf02ea03dab64320c9c2d51f85fb987a184033f
Abhinn Mishra <aviabhinn98@gmail.com>
2021-05-20 12:00:24 +0530

    AsyncAwait

---
+++ Threading/AsyncAndAwait.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Threading;
+
+namespace CSharpHandsOnPractice.Threading
+{
+    class AsyncAndAwait
+    {
+        static void Main(string[] args)
+        {
+            Method();
+            Console.WriteLine("Main thread");
+            Console.ReadLine();
+        }
+        
+        public static async void Method()
+        {
+            await Task.Run(new Action(LongTask));
+            Console.WriteLine("New Thread");
+        }
+        Public static void LongTask()
+        {
+            Thread.Sleep(20000);
+        }
+    }
+    
+}
