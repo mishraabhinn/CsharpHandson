8ad3920ab49fc60392c3f6334327c33135d94274
Abhinn Mishra <aviabhinn98@gmail.com>
2021-05-26 09:49:14 +0530

    Xunit

---
+++ XunitTesting/PracticeUnitTesting/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace abhinn
+{
+    public class Program
+    {
+        public bool isvalid(string s1,string s2 )
+        {
+            if (s1 == s2)
+                return true;
+            else
+                return false;
+        }
+        static void Main(string[] args)
+        {
+
+            //var obj = new Program();
+            //bool ans =obj.isvalid("s1", "s1");
+            //Console.WriteLine(ans);
+            
+            string author1 = "s1";
+            string author2 = "s2";
+
+            // Use String.Compare method  
+            if (String.Compare(author1, author2) == 0)
+                Console.WriteLine($"Both strings have same value.");
+            else if (String.Compare(author1, author2) < 0)
+                Console.WriteLine($"{author1} precedes {author2}.");
+            else if (String.Compare(author1, author2) > 0)
+                Console.WriteLine($"{author1} follows {author2}.");
+
+
+        }
+    }
+}
---
+++ XunitTesting/xUnittest/UnitTest1.cs
@@ -0,0 +1,21 @@
+using System;
+using Xunit;
+using abhinn;
+
+namespace xUnittest
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+            //arrange
+            var obj = new Program();
+            bool expected = false; 
+            //act
+            bool actual = obj.isvalid("s1", "s1");
+            //assert
+            Assert.Equal(expected, actual);
+        }
+    }
+}
